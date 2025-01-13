5f397356c93a6c66c0fffabe492f30ec3460b950
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2025-01-13 12:06:54 -0300

    goodprint

---
+++ api.js
@@ -0,0 +1,31 @@
+const url = " https://economia.awesomeapi.com.br/last/XAU-USD"
+const urlBr = "https://economia.awesomeapi.com.br/last/USD-BRL"
+const inputPrice = document.querySelector("#VG")
+
+
+
+const gold = async (url, urlLastUp) => {
+   const  response = await fetch(url)
+   const data =  await response.json()
+   const troy = (Number(data['XAUUSD'].ask)+Number(data['XAUUSD'].bid))/2
+   const grama = troy/31.1035
+
+
+
+   const responseBrl = await fetch(urlLastUp)
+   
+   const datas = await responseBrl.json() 
+   const sellPrice = datas[`USDBRL`].bid
+   const callPrice = datas[`USDBRL`].ask
+   const basePrice = ((Number(sellPrice) + Number(callPrice)) / 2).toFixed(2)
+
+
+   let priceGrama = grama*basePrice
+   inputPrice.value = priceGrama.toFixed(2)
+   
+   
+
+    
+}
+
+gold(url,urlBr)
---
+++ app.js
@@ -0,0 +1,62 @@
+
+const valorDaGrama = document.querySelector("#VG")
+const spread = document.querySelector("#SP")
+const quantidade = document.querySelector("#QG")
+const custoDoFrete = document.querySelector("#FR")
+const button = document.querySelector(".bt")
+const divContainers = document.querySelector(".enters")
+const allPs = document.querySelectorAll("p")
+const divContainerFormanipulatiom = divContainers.children
+let valorFinalGram = document.querySelector("#valorgramafi")
+let valorFreteFinal = document.querySelector("#valorfinalfrete")
+let total = document.querySelector("#total")
+const buttonPrint = document.querySelector("#buttonPrint")
+const buttonRetry = document.querySelector("#retray")
+const formatoBRL = new Intl.NumberFormat('pt-BR', { 
+    style: 'currency', 
+    currency: 'BRL' 
+  });
+
+button.addEventListener("click", ()=> {
+    const price = Number(valorDaGrama.value) * (((Number(spread.value)/100)) + 1) * Number(quantidade.value)
+    
+    valorFinalGram.innerText = formatoBRL.format((Number(valorDaGrama.value) * (((Number(spread.value)/100)) + 1)))
+    
+    
+    valorFreteFinal.innerText = formatoBRL.format(Number(custoDoFrete.value))
+
+
+     total.innerText = formatoBRL.format((price+Number(custoDoFrete.value)))
+
+    
+
+    Array.from(divContainerFormanipulatiom).forEach( p => p.classList.toggle("showitemoff"))
+
+    Array.from(document.querySelectorAll(".buttonnsfim")).forEach(b => b.classList.toggle("showitemoff"))
+
+    
+
+    
+})
+
+
+
+buttonRetry.addEventListener("click", () => {
+
+    Array.from(divContainerFormanipulatiom).forEach( p => p.classList.toggle("showitemoff"))
+
+    Array.from(document.querySelectorAll(".buttonnsfim")).forEach(b => b.classList.toggle("showitemoff"))
+
+})
+
+
+buttonPrint.addEventListener('click', () => {
+    const element = document.getElementById('print');
+    html2canvas(element).then((canvas) => {
+      // Baixar a imagem
+      const link = document.createElement('a');
+      link.download = 'screenshot.png';
+      link.href = canvas.toDataURL('image/png');
+      link.click();
+    });
+  });
